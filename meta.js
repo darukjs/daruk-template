94c0a9d690a43f8096cf28dcd7797ca83be78392
Youjingyu <735284268@qq.com>
2018-09-03 19:19:28 +0800

    更新模板

---
+++ meta.js
@@ -0,0 +1,155 @@
+const path = require('path')
+
+const {
+  sortDependencies,
+  installDependencies,
+  runLintFix,
+  printMessage
+} = require('./utils')
+
+const templateVersion = require('./package.json').version
+
+module.exports = {
+  metalsmith: {
+    before (metalsmith) {
+      Object.assign(
+        metalsmith.metadata(),
+        { isNotTest: true }
+      )
+    }
+  },
+  helpers: {
+    if_or (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this)
+      }
+
+      return options.inverse(this)
+    },
+    template_version () {
+      return templateVersion
+    }
+  },
+  prompts: {
+    name: {
+      when: 'isNotTest',
+      type: 'string',
+      required: true,
+      message: 'Project name'
+    },
+    description: {
+      when: 'isNotTest',
+      type: 'string',
+      required: false,
+      message: 'Project description',
+      default: 'A daruk project'
+    },
+    author: {
+      when: 'isNotTest',
+      type: 'string',
+      message: 'Author'
+    },
+    features: {
+      when: 'isNotTest',
+      type: 'checkbox',
+      message: 'select the features you need',
+      choices: [
+        {
+          name: 'mysql',
+          value: 'mysql',
+          short: 'mysql',
+          description: 'mysql',
+          link: 'https://github.com/mysqljs/mysql',
+          checked: false
+        },
+        {
+          name: 'ioredis',
+          value: 'ioredis',
+          short: 'ioredis',
+          description: 'ioredis',
+          link: 'https://github.com/luin/ioredis',
+          checked: false
+        }
+      ]
+    },
+    lint: {
+      when: 'isNotTest',
+      type: 'confirm',
+      message: 'Use ESLint to lint your code?'
+    },
+    lintConfig: {
+      when: 'isNotTest && lint',
+      type: 'list',
+      message: 'Pick an ESLint preset',
+      choices: [
+        {
+          name: 'Standard (https://github.com/standard/standard)',
+          value: 'standard',
+          short: 'Standard'
+        },
+        {
+          name: 'Airbnb (https://github.com/airbnb/javascript)',
+          value: 'airbnb',
+          short: 'Airbnb'
+        },
+        {
+          name: 'none (configure it yourself)',
+          value: 'none',
+          short: 'none'
+        }
+      ]
+    },
+    autoInstall: {
+      when: 'isNotTest',
+      type: 'list',
+      message:
+        'Should we run `npm install` for you after the project has been created? (recommended)',
+      choices: [
+        {
+          name: 'Yes, use NPM',
+          value: 'npm',
+          short: 'npm'
+        },
+        {
+          name: 'Yes, use Yarn',
+          value: 'yarn',
+          short: 'yarn'
+        },
+        {
+          name: 'No, I will handle that myself',
+          value: false,
+          short: 'no'
+        }
+      ]
+    }
+  },
+  filters: {
+    '.eslintrc.js': 'lint',
+    '.eslintignore': 'lint',
+    'config/test.env.js': 'unit || e2e',
+    'build/webpack.test.conf.js': "unit && runner === 'karma'",
+    'src/router/**/*': 'router'
+  },
+  complete: function (data, { chalk }) {
+    const green = chalk.green
+
+    sortDependencies(data, green)
+
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+    if (data.autoInstall) {
+      installDependencies(cwd, data.autoInstall, green)
+        .then(() => {
+          return runLintFix(cwd, data, green)
+        })
+        .then(() => {
+          printMessage(data, green)
+        })
+        .catch(e => {
+          console.log(chalk.red('Error:'), e)
+        })
+    } else {
+      printMessage(data, chalk)
+    }
+  }
+}
