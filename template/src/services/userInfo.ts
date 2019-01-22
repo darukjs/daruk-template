e94c73f3f12b3d35f6021415731dfc02ea9b1b45
xiaojue <designsor@gmail.com>
2019-01-22 13:48:17 +0800

    update vscode config

--- template/src/services/userInfo.ts
+++ template/src/services/userInfo.ts
@@ -6,7 +6,7 @@
 import { BaseService } from '@sina/daruk';
 
 export default class MyService extends BaseService {
-  getUserList() {
+  public getUserList() {
     const { mysql } = this.ctx.plugin;
     return mysql.queryUserList();
   }
