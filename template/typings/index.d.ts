b0943e0320d81257674e0506c1bba878913aab4b
xiaojue <designsor@gmail.com>
2019-01-23 14:05:24 +0800

    add types

--- template/typings/index.d.ts
+++ template/typings/index.d.ts
@@ -1,8 +1,9 @@
 declare module '{{name}}' {
-  export class MyClass {
-    public constructor(initData: any);
+  class MyClass {
+    public constructor(initData?: any);
     public map: any;
     public get(key: string): any;
     public set(key: string, val: any): any;
   }
+  export = MyClass;
 }
